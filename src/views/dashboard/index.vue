aaa9380ffd40ae0f505a66619bea42fc71a4e5e7
csycsx <chensiyu_990710@126.com>
2023-04-27 16:34:15 +0800

    首页修改

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">工号: {{ id }}</div>
+    <!-- <div class="dashboard-text">工号: {{ id }}</div>
     <div class="dashboard-text">姓名: {{ name }}</div>
-    <div class="dashboard-text">部门: {{ yuanxi }}</div>
+    <div class="dashboard-text">部门: {{ yuanxi }}</div> -->
+    <div>请点击以下链接，阅读学校请假和考勤相关规定：</div>
     <div class="foot" style="color:#f0000f">
       <a href="https://hr.shu.edu.cn/info/1042/4217.htm" slot="reference">上海大学教职工请假和考勤制度的规定</a>
     </div>
-    <el-row type="flex" justify="center" align="middle">
-      <el-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8">
-          <el-card class="box-card">
-            <h2>说明</h2>
-            <p>1. 请假类型包括：事假、病假、婚假、产假、丧假、因公出差、工伤假</p>
-            <p>2. 事假包含因私出国（境）</p>
-            <p>3. 产假包含男方配偶陪产假</p>
-            <p>4. 因公出差包含因公外借、挂职锻炼、公派出国。公派出国需在系统关联PIM中已完成的因公出国（境）申请流程</p>
-            <p>5. 中层干部及其他报备人员仍在PIM系统提交流程，包括：PIM中层干部及其他报备人员因私出国(境)审批流程、二级单位主要负责同志外出请假流程、非二级单位主要负责同志外出请假流程。</p>
-          </el-card>
-        </el-col>
+    <h3>业务说明</h3>
+    <el-row type="flex" justify="center">
+      <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" type="flex">
+
+        <p>1. 请假类型包括：事假、病假、婚假、产假、丧假、因公出差、工伤假</p>
+        <p>2. 事假包含因私出国（境）</p>
+        <p>3. 产假包含男方配偶陪产假</p>
+        <p>4. 因公出差包含因公外借、挂职锻炼、公派出国。公派出国需在系统关联PIM中已完成的因公出国（境）申请流程</p>
+        <p>5. 中层干部及其他报备人员仍在PIM系统提交流程，包括：PIM中层干部及其他报备人员因私出国(境)审批流程、二级单位主要负责同志外出请假流程、非二级单位主要负责同志外出请假流程。</p>
+
+      </el-col>
     </el-row>
     <!-- <div class="dashboard-text">身份权限: {{ role }}</div> -->
   </div>
@@ -24,7 +25,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import { example} from '@/api/example'
+import { example } from '@/api/example'
 
 export default {
   name: 'Dashboard',
@@ -35,21 +36,21 @@ export default {
       'yuanxi'
     ])
   },
-  created(){
+  created () {
     /*
       data 对象是存储在前端的数据，一般请求的时候需要这个信息做验证查验，
       example调用的接口是get，所以没有传回后端
     */
     var data = {
-      id : this.$store.getters.id , // 获取 store中的 id
-      name : this.$store.getters.name , // 获取 store中的 name
-      gender : this.$store.getters.gender , // 获取 store中的 gender
-      role : this.$store.getters.role , // 获取 store中的 role
-      yuanxi : this.$store.getters.yuanxi , // 获取 store中的 yuanxi 院系
-      zyjszw : this.$store.getters.zyjszw,
-      zyjsdj : this.$store.getters.zyjsdj,
-      xzzj : this.$store.getters.xzzj,
-      glgwzw : this.$store.getters.glgwzw,
+      id: this.$store.getters.id, // 获取 store中的 id
+      name: this.$store.getters.name, // 获取 store中的 name
+      gender: this.$store.getters.gender, // 获取 store中的 gender
+      role: this.$store.getters.role, // 获取 store中的 role
+      yuanxi: this.$store.getters.yuanxi, // 获取 store中的 yuanxi 院系
+      zyjszw: this.$store.getters.zyjszw,
+      zyjsdj: this.$store.getters.zyjsdj,
+      xzzj: this.$store.getters.xzzj,
+      glgwzw: this.$store.getters.glgwzw,
       gw: this.$store.getters.gw
     }
     console.log("这里存储的是用户信息")
@@ -60,7 +61,7 @@ export default {
     //   console.log("后端传回的数据")
     //   console.log(response)
     // })
-  
+
   }
 }
 </script>
@@ -76,12 +77,17 @@ export default {
   }
 }
 .dashboard-container {
-  text-align: center;
+  // text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 .foot {
-  width: 300px;
   height: 20px;
   margin: 10px auto;
+  font-size: 1.5em;
   text-decoration: underline;
+  font-weight: bold;
+  margin-bottom: 25px;
 }
 </style>
