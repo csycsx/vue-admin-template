27fa85cce1bfa1842013e5b95acf182d630be007
lyzeatpotato <850209171@qq.com>
2023-07-09 01:24:21 +0800

    0707

--- src/views/audit/attendance/SummaryAudit.vue
+++ src/views/audit/attendance/SummaryAudit.vue
@@ -5,56 +5,56 @@
       <!-- 审核步骤 -->
       <div class="box-step">
         <el-steps :active=active finish-status="success" align-center>
-          <el-step v-if="step>1" title="部门审核"></el-step>
-          <el-step v-if="step>2" title="人事处初审"></el-step>
+          <el-step v-if="step > 1" title="部门审核"></el-step>
+          <el-step v-if="step > 2" title="人事处初审"></el-step>
           <!-- <el-step v-if="step>1" title="人事处审核"></el-step> -->
         </el-steps>
       </div>
       <!-- 请假信息表单 -->
-   <!-- 表格 -->
-   <div class="table">
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <h3>{{ this.dept }} {{this.month}}月考勤记录</h3>
-        </div>
-        <div class="kaoqing">
-          <el-table :data="tableData"  border style="">   
-            <el-table-column label="编号" property="id"  ></el-table-column>
-            <el-table-column label="工号" prop="user.id"   ></el-table-column>
-            <el-table-column label="事假" prop="shijiaDays"  ></el-table-column>
-            <el-table-column label="病假" prop="bingjiaDays"   ></el-table-column>
-            <el-table-column label="婚假" prop="hunjiaDays"  ></el-table-column>
-            <el-table-column label="生育假" prop="shengyujiaDays" ></el-table-column>
-            <el-table-column label="探亲假" prop="tanqinjiaDays" ></el-table-column>
-            <el-table-column label="丧假" prop="sangjiaDays" ></el-table-column>
-            <el-table-column label="公差" prop="gongchaiDays"  ></el-table-column>
-            <el-table-column label="旷工" prop="kuanggongDays"  ></el-table-column>
-            <el-table-column label="其它" prop="inactiveDays" ></el-table-column>
-            <el-table-column label="备注" prop="leaveReason" ></el-table-column>
-          </el-table>
-        </div>
-        <!-- <div class="block">
+      <!-- 表格 -->
+      <div class="table">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <h3>{{ this.dept }} {{ this.month }}月考勤记录</h3>
+          </div>
+          <div class="kaoqing">
+            <el-table :data="tableData" border style="">
+              <el-table-column label="编号" property="id"></el-table-column>
+              <el-table-column label="工号" prop="user.id"></el-table-column>
+              <el-table-column label="事假" prop="shijiaDays"></el-table-column>
+              <el-table-column label="病假" prop="bingjiaDays"></el-table-column>
+              <el-table-column label="婚假" prop="hunjiaDays"></el-table-column>
+              <el-table-column label="生育假" prop="shengyujiaDays"></el-table-column>
+              <el-table-column label="探亲假" prop="tanqinjiaDays"></el-table-column>
+              <el-table-column label="丧假" prop="sangjiaDays"></el-table-column>
+              <el-table-column label="公差" prop="gongchaiDays"></el-table-column>
+              <el-table-column label="旷工" prop="kuanggongDays"></el-table-column>
+              <el-table-column label="其它" prop="inactiveDays"></el-table-column>
+              <el-table-column label="备注" prop="leaveReason"></el-table-column>
+            </el-table>
+          </div>
+          <!-- <div class="block">
           <el-pagination @current-change="handleCurrentChange" :current-page="currentPage" :page-size="pageSize"
             layout="total, prev, pager, next, jumper" :total="total">
           </el-pagination>
         </div> -->
-      </el-card>
-    </div>
-     <!-- 审核详情 -->
+        </el-card>
+      </div>
+      <!-- 审核详情 -->
       <div class="detail-box">
         <!-- <el-button type="text" @click="dialogVisible = true">点击查看审核详情</el-button> -->
         <el-dialog title="审核详情" :visible.sync="dialogVisible" width="60%" :before-close="handleClose">
           <el-collapse v-model="activeNames" @change="handleChange">
-            <el-collapse-item title="部门人事初审信息" name="1" v-if="active>0">
+            <el-collapse-item title="部门人事初审信息" name="1" v-if="active > 0">
               <StepInfo :step-info="stepInfo1" />
             </el-collapse-item>
-            <el-collapse-item title="部门负责人审核信息" name="2" v-if="active>0">
+            <el-collapse-item title="部门负责人审核信息" name="2" v-if="active > 0">
               <StepInfo :step-info="stepInfo2" />
             </el-collapse-item>
-            <el-collapse-item title="人事处科员初审信息" name="3" v-if="active>2">
+            <el-collapse-item title="人事处科员初审信息" name="3" v-if="active > 2">
               <StepInfo :step-info="stepInfo3" />
             </el-collapse-item>
-            <el-collapse-item title="人事处负责人审核信息" name="4" v-if="active>3">
+            <el-collapse-item title="人事处负责人审核信息" name="4" v-if="active > 3">
               <StepInfo :step-info="stepInfo4" />
             </el-collapse-item>
             <!-- <el-collapse-item title="校领导审核信息" name="4" v-if="active>4">
@@ -81,7 +81,7 @@
           </el-form-item>
         </el-form>
       </div>
-      <div class="check-finish" v-if="isFinish===1 ">
+      <div class="check-finish" v-if="isFinish === 1">
         <i class="el-icon-success"></i>
         温馨提示：您已完成审核，无需再审核！
       </div>
@@ -101,7 +101,7 @@ import { checkTeachingDate } from "@/api/apply";
 export default {
   // props: ['info'],
   components: { StepInfo },
-  data () {
+  data() {
     return {
       role: "",
       userid: "",
@@ -112,7 +112,7 @@ export default {
         result: "",
         recomment: "",
       },
-      
+
       isShow: 0,
       isFinish: 0,
       dialogVisible: false,
@@ -126,11 +126,11 @@ export default {
       // changeTime: "",
       leaveTime: "",
 
-      year:"",
-      month:"",
-      dept:"",
+      year: "",
+      month: "",
+      dept: "",
 
-      tableData:[],
+      tableData: [],
 
       currentPage: 1,
       total: 0,
@@ -138,22 +138,22 @@ export default {
 
     }
   },
-  created () {
+  created() {
     let yuanxi = this.$store.getters.yuanxi
     this.role = this.$store.getters.role_num;
 
     this.userid = this.$store.getters.id;
     this.name = this.$store.getters.name;
     // this.dept = this.$store.getters.yuanxi;
-    console.log("this.$store.getters",this.$store.getters)
+    console.log("this.$store.getters", this.$store.getters)
     // this.year = "2023";
     // this.month = "5"  
-     
+
 
   },
-  mounted () {
+  mounted() {
     this.info = JSON.parse(window.sessionStorage.getItem('attendanceDetail'));
-    console.log("@@@info&role",this.info, this.role)
+    console.log("@@@info&role", this.info, this.role)
 
     this.info1 = JSON.parse(window.sessionStorage.getItem('hAttendanceDetail'));
 
@@ -161,47 +161,47 @@ export default {
     this.init();
   },
   methods: {
-   
-    getDeptList(){
-      this.year=this.info.year
-        this.month=this.info.month
-        this.dept=this.info.department
+
+    getDeptList() {
+      this.year = this.info.year
+      this.month = this.info.month
+      this.dept = this.info.department
       getLeaveHistoryByDept({
         "year": this.year,
         "month": this.month,
-        "dept": this.dept 
+        "dept": this.dept
       }).then(res => {
-        console.log(" getLeaveHistoryByDeptres",res);
+        console.log(" getLeaveHistoryByDeptres", res);
         this.tableData = res.data.records
-        console.log("this.info.year",this.info.year);
+        console.log("this.info.year", this.info.year);
 
-        
+
       })
 
-  },
-  mounted () {
-    this.info = JSON.parse(window.sessionStorage.getItem('summaryDetail'));
-    console.log(this.info)
-    this.init();
-  },
-  methods: {
-    handleChange (val) {
-      console.log(val);
     },
-    handleClose (done) {
-      this.dialogVisible = false;
+    mounted() {
+      this.info = JSON.parse(window.sessionStorage.getItem('summaryDetail'));
+      console.log(this.info)
+      this.init();
     },
+    methods: {
+      handleChange(val) {
+        console.log(val);
+      },
+      handleClose(done) {
+        this.dialogVisible = false;
+      },
 
-    onSubmit () {
-      console.log("this.check.result",this.check.result)
-      if (this.check.result === "不通过" && this.check.recomment === "") {
-        this.$notify.error({
-          title: '错误',
-          message: '审核不通过请填写理由'
-        });
-      }
-      else {
-        let formData = new FormData();
+      onSubmit() {
+        console.log("this.check.result", this.check.result)
+        if (this.check.result === "不通过" && this.check.recomment === "") {
+          this.$notify.error({
+            title: '错误',
+            message: '审核不通过请填写理由'
+          });
+        }
+        else {
+          let formData = new FormData();
           formData.append("year", this.year);
           formData.append("month", this.month);
           formData.append("dept", this.$store.getters.yuanxi);
@@ -220,65 +220,65 @@ export default {
               })
             }
           })
-      }
-    },
-
-    init () {
-      //判断是否能审核
-      console.log("showStatus", this.info.showStatus)
-      if (this.info.showStatus === "已审核") {
-        this.isShow = 1;
-        this.isFinish = 1;
-      }
-      this.getDetail()
-
-    },
-    getDetail () {
-      getCurrentAuditMsg({ "leave_id": this.info.id }).then(res => {
-        console.log(res);
-        if (res.code === 200) {
-          if (res.data.departmentAuditMsg !== "尚未进行部门审核") {
-            this.stepInfo1.id = res.data.departmentAuditMsg.dpOfficerId;
-            this.stepInfo1.name = res.data.departmentAuditMsg.dpOfficerName;
-            this.stepInfo1.result = res.data.departmentAuditMsg.dpOfficerResult;
-            this.stepInfo1.recommend = res.data.departmentAuditMsg.dpOfficerRecommend;
-            this.stepInfo1.time = res.data.departmentAuditMsg.dpOfficerTime;
-            this.stepInfo2.id = res.data.departmentAuditMsg.dpLeaderId;
-            this.stepInfo2.name = res.data.departmentAuditMsg.dpLeaderName;
-            this.stepInfo2.result = res.data.departmentAuditMsg.dpLeaderResult;
-            this.stepInfo2.recommend = res.data.departmentAuditMsg.dpLeaderRecommend;
-            this.stepInfo2.time = res.data.departmentAuditMsg.dpLeaderTime;
-          }
-          if (this.step > 2 && res.data.hrAuditMsg !== "尚未进行人事处审核") {
-            this.stepInfo3.id = res.data.hrAuditMsg.hrOfficerId;
-            this.stepInfo3.name = res.data.hrAuditMsg.hrOfficerName;
-            this.stepInfo3.result = res.data.hrAuditMsg.hrOfficerResult;
-            this.stepInfo3.recommend = res.data.hrAuditMsg.hrOfficerRecommend;
-            this.stepInfo3.time = res.data.hrAuditMsg.hrOfficerTime;
-            this.stepInfo4.id = res.data.hrAuditMsg.hrLeaderId;
-            this.stepInfo4.name = res.data.hrAuditMsg.hrLeaderName;
-            this.stepInfo4.result = res.data.hrAuditMsg.hrLeaderResult;
-            this.stepInfo4.recommend = res.data.hrAuditMsg.hrLeaderRecommend;
-            this.stepInfo4.time = res.data.hrAuditMsg.hrLeaderTime;
-          }
-          // if (this.step > 4 && res.data.schoolAuditMsg !== "尚未进行校领导审核") {
-          //   this.stepInfo5.id = res.data.schoolAuditMsg.scOfficerId;
-          //   this.stepInfo5.name = res.data.schoolAuditMsg.scLeaderName;
-          //   this.stepInfo5.result = res.data.schoolAuditMsg.scOfficerResult;
-          //   this.stepInfo5.recommend = res.data.schoolAuditMsg.scOfficerRecommend;
-          //   this.stepInfo5.time = res.data.schoolAuditMsg.scOfficerTime;
-          // }
         }
-      })
-    },
-     handleChange (val) {
-      console.log(val);
-    },
-        handleClose (done) {
-      this.dialogVisible = false;
-    },
-  
-    changeDateFormat (date) {
+      },
+
+      init() {
+        //判断是否能审核
+        console.log("showStatus", this.info.showStatus)
+        if (this.info.showStatus === "已审核") {
+          this.isShow = 1;
+          this.isFinish = 1;
+        }
+        this.getDetail()
+
+      },
+      getDetail() {
+        getCurrentAuditMsg({ "leave_id": this.info.id }).then(res => {
+          console.log(res);
+          if (res.code === 200) {
+            if (res.data.departmentAuditMsg !== "尚未进行部门审核") {
+              this.stepInfo1.id = res.data.departmentAuditMsg.dpOfficerId;
+              this.stepInfo1.name = res.data.departmentAuditMsg.dpOfficerName;
+              this.stepInfo1.result = res.data.departmentAuditMsg.dpOfficerResult;
+              this.stepInfo1.recommend = res.data.departmentAuditMsg.dpOfficerRecommend;
+              this.stepInfo1.time = res.data.departmentAuditMsg.dpOfficerTime;
+              this.stepInfo2.id = res.data.departmentAuditMsg.dpLeaderId;
+              this.stepInfo2.name = res.data.departmentAuditMsg.dpLeaderName;
+              this.stepInfo2.result = res.data.departmentAuditMsg.dpLeaderResult;
+              this.stepInfo2.recommend = res.data.departmentAuditMsg.dpLeaderRecommend;
+              this.stepInfo2.time = res.data.departmentAuditMsg.dpLeaderTime;
+            }
+            if (this.step > 2 && res.data.hrAuditMsg !== "尚未进行人事处审核") {
+              this.stepInfo3.id = res.data.hrAuditMsg.hrOfficerId;
+              this.stepInfo3.name = res.data.hrAuditMsg.hrOfficerName;
+              this.stepInfo3.result = res.data.hrAuditMsg.hrOfficerResult;
+              this.stepInfo3.recommend = res.data.hrAuditMsg.hrOfficerRecommend;
+              this.stepInfo3.time = res.data.hrAuditMsg.hrOfficerTime;
+              this.stepInfo4.id = res.data.hrAuditMsg.hrLeaderId;
+              this.stepInfo4.name = res.data.hrAuditMsg.hrLeaderName;
+              this.stepInfo4.result = res.data.hrAuditMsg.hrLeaderResult;
+              this.stepInfo4.recommend = res.data.hrAuditMsg.hrLeaderRecommend;
+              this.stepInfo4.time = res.data.hrAuditMsg.hrLeaderTime;
+            }
+            // if (this.step > 4 && res.data.schoolAuditMsg !== "尚未进行校领导审核") {
+            //   this.stepInfo5.id = res.data.schoolAuditMsg.scOfficerId;
+            //   this.stepInfo5.name = res.data.schoolAuditMsg.scLeaderName;
+            //   this.stepInfo5.result = res.data.schoolAuditMsg.scOfficerResult;
+            //   this.stepInfo5.recommend = res.data.schoolAuditMsg.scOfficerRecommend;
+            //   this.stepInfo5.time = res.data.schoolAuditMsg.scOfficerTime;
+            // }
+          }
+        })
+      },
+      handleChange(val) {
+        console.log(val);
+      },
+      handleClose(done) {
+        this.dialogVisible = false;
+      },
+
+      changeDateFormat(date) {
         var myDate = new Date(date);
         var Y = myDate.getFullYear(); //获取当前完整年份
         var M = myDate.getMonth() + 1; //获取当前月份
@@ -292,7 +292,8 @@ export default {
         var nowDate = Y + "-" + M + "-" + D;
         return nowDate;
       },
-  },
+    },
+  }
 }
 </script>
 
@@ -301,6 +302,7 @@ export default {
   /* background-color: #f5f7fa; */
   padding: 16px;
 }
+
 .box {
   display: flex;
   background: #ffffff;
@@ -309,24 +311,29 @@ export default {
   flex-direction: column;
   align-items: center;
 }
+
 .box-title {
   margin: 0;
   padding: 20px;
   text-align: center;
 }
+
 .box-step {
   margin-top: 20px;
   width: 80%;
 }
-.kaoqing{
+
+.kaoqing {
   position: relative;
   left: -92px;
 }
+
 .clearfix:before,
 .clearfix:after {
   display: table;
   content: "";
 }
+
 .clearfix:after {
   clear: both;
 }
@@ -334,6 +341,7 @@ export default {
 .table {
   margin-top: 20px;
 }
+
 .box-card ::v-deep .el-card__header {
   border-bottom: 1.5px solid #ebeef5;
   width: 100%;
@@ -351,10 +359,12 @@ export default {
   margin: 0 20px;
   padding-left: 10rem;
 }
+
 .info-box {
   width: 100%;
   padding: 20px;
 }
+
 .row-box {
   margin-bottom: 20px;
 }
@@ -363,10 +373,12 @@ export default {
   font-size: 14px;
   font-weight: 600;
 }
+
 .content-box {
   color: #5c5a5a;
   font-weight: 600;
 }
+
 .check-box {
   width: 80%;
 }
@@ -375,17 +387,20 @@ export default {
   width: 80%;
   color: red;
 }
+
 .check-finish {
   width: 80%;
   color: #67c23a;
 }
+
 .detail-box::v-deep .el-dialog__title {
   font-weight: 700;
 }
+
 .el-collapse::v-deep .el-collapse-item__header {
   font-weight: 700;
 }
+
 .el-collapse::v-deep .el-collapse-item__content {
   padding-bottom: 0;
-}
-</style>
+}</style>
